211f641d03a1882ff677f64456826147d6e67f37
Nghi Tran <nghi.tran@veriksystems.com>
2022-05-06 01:02:53 +0700

    Initial Commit

---
+++ ArloTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArloTest
+//
+//  Created by Nghi Tran on 5/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
